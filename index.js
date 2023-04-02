61de7ada50310d8a765812cf652bb1f575c1ec6a
Xue64 <rufelleemmanuelpactol7@gmail.com>
2023-04-02 23:21:16 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", ()=>main())
+
+
+
+
+
+function main ()
+{
+    const head = document.getElementById('head_name')
+    const github = document.getElementById('github_logo')
+}
+
+
+
+
